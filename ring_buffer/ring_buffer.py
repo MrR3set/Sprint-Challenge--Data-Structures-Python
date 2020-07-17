cfa79dcb29ae71e5c0d2fc6b64ebaeb041ce5cb8
MrR3set <bairon.pazrobles@gmail.com>
2020-07-17 09:26:29 -0700

    ringbuffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.index = 0 #This will help see what element should be removed
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage.pop(self.index)
+            self.storage.insert(self.index,item)
+            self.index+=1
+            if self.index >= self.capacity:
+                self.index = 0
 
     def get(self):
-        pass
+        return self.storage
+
+
+
+# rb = RingBuffer(3)
+
+# rb.append("a")
+# rb.append("b")
+# rb.append("c")
+# rb.get()
+# rb.append("d")
+# rb.get()
+# rb.append('e')
+# rb.append('f')
+# rb.get()
